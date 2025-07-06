efd10ae117c3afef1db7d8b42996210cb2c422c5
Sajal Shrestha <sajalmshrestha@gmail.com>
2025-07-06 14:54:09 -0700

    backend user session

--- movie_test_3.py
+++ movie_test_3.py
@@ -245,7 +245,7 @@ def suggest_corrections(query, search_results):
                                 "poster_path": movie.get('poster_path', ''),
                                 "id": movie['id']
                             })
-                            st.session_state["search_done"] = True  # ✅ matches old version behavior
+                            st.session_state["search_done"] = True  # ✅ Hide Top 5
                             st.success(f"✅ Added {movie['title']}")
                             st.rerun()
             
