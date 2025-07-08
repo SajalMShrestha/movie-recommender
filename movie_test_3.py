dd7ed0e0e396fda8cef9a1ffd57d6ba166337433
Sajal Shrestha <sajalmshrestha@gmail.com>
2025-07-08 11:09:46 -0700

    final prelaunch

--- movie_test_3.py
+++ movie_test_3.py
@@ -2060,7 +2060,7 @@ if st.session_state.recommend_triggered:
         st.markdown("---")
         st.subheader("📧 Email")
         save_email = st.text_input(
-            "",
+        
             placeholder="your.email@example.com",
             key="save_profile_email"
         )
